f302df8b93eba6547c527c507a7ca5219fb26365
KIRAN KOMMULA <kiran@gmail.com>
2023-10-31 16:11:55 +0530

    pov

---
+++ src/main/java/com/pack/pov/PovApplication.java
@@ -0,0 +1,13 @@
+package com.pack.pov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PovApplication.class, args);
+	}
+
+}
