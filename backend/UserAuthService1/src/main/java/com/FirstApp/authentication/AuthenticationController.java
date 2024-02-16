0a4d75924fdaf2bebb19e7969f1d55b68d764bca
RAJ <107236972+raju1409@users.noreply.github.com>
2024-02-16 07:07:23 +0000

    Instructions included

--- backend/UserAuthService1/src/main/java/com/FirstApp/authentication/AuthenticationController.java
+++ backend/UserAuthService1/src/main/java/com/FirstApp/authentication/AuthenticationController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +39,10 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+    @GetMapping("/test")
+    public String testMethod(){
+        return "Test Endpoint";
     }
 
 }
